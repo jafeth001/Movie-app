8804d2f7352c90c449af4690a61242fd5a7de5fa
jafeth001 <115637000+jafeth001@users.noreply.github.com>
2024-02-12 15:27:05 +0300

    Create README.md

--- springfordemo/src/main/java/com/backendengineer/springfordemo/Repository/MovieRepository.java
+++ springfordemo/src/main/java/com/backendengineer/springfordemo/Repository/MovieRepository.java
@@ -4,9 +4,7 @@ import com.backendengineer.springfordemo.Entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface MovieRepository extends JpaRepository<Movie,Long> {
+
 }
