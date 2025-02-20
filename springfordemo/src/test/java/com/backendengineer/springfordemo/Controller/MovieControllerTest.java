ba88572308ffaeb9ec53cbcbc7c71fec735440f4
jafeth001 <jafethadet2021@gmail.com>
2025-02-20 13:18:29 +0300

    add Unit testing

--- springfordemo/src/test/java/com/backendengineer/springfordemo/Controller/MovieControllerTest.java
+++ springfordemo/src/test/java/com/backendengineer/springfordemo/Controller/MovieControllerTest.java
@@ -5,6 +5,7 @@ import com.backendengineer.springfordemo.Service.MovieService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -43,7 +47,8 @@ class MovieControllerTest {
     }
 
     @Test
-    void addMovie() throws Exception {
+    @DisplayName("Junit test for saving movie")
+    void whenAddMovie_thenReturnMovie() throws Exception {
         Mockito.when(movieService.addMovie(movie)).thenReturn(movie);
         mockMvc.perform(MockMvcRequestBuilders.post("/movie/add")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -53,14 +58,44 @@ class MovieControllerTest {
     }
 
     @Test
-    void findallmovie() {
+    @DisplayName("Junit test for finding all movies")
+    void whenFindAll_thenReturnList() throws Exception {
+        var newMovie = Movie.builder()
+                .id(2L)
+                .title("Ramster")
+                .description("A ramster-bending thriller")
+                .description("test")
+                .poster("poster.jpg")
+                .build();
+        Mockito.when(movieService.addMovie(movie)).thenReturn(movie);
+        mockMvc.perform(MockMvcRequestBuilders.get("/movie/all")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(List.of(movie, newMovie))))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(print());
     }
 
     @Test
-    void findbyid() {
+    @DisplayName("Junit test for finding movie by id")
+    void whenFindById_thenReturnMovie() throws Exception {
+        Long movieId = 1L;
+        Mockito.when(movieService.findbyid(movieId)).thenReturn(java.util.Optional.ofNullable(movie));
+        mockMvc.perform(MockMvcRequestBuilders.get("/movie/{id}", movieId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(movieId)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(print());
     }
 
     @Test
-    void deletebyid() {
+    @DisplayName("Junit test for deleting movie by id")
+    void whenDeleteMovie_thenReturnMovie() throws Exception {
+        Long movieId = 1L;
+        Mockito.when(movieService.findbyid(movieId)).thenReturn(java.util.Optional.ofNullable(movie));
+        mockMvc.perform(MockMvcRequestBuilders.delete("/movie/{id}", movieId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(movieId)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(print());
     }
 }
