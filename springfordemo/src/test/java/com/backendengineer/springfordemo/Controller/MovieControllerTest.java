d0aed6f307ebc0c100dd24e7346a18bc0e20d2d6
jafeth001 <jafethadet2021@gmail.com>
2025-02-20 10:39:40 +0300

    add Unit testing

--- springfordemo/src/test/java/com/backendengineer/springfordemo/Controller/MovieControllerTest.java
+++ springfordemo/src/test/java/com/backendengineer/springfordemo/Controller/MovieControllerTest.java
@@ -1,11 +1,22 @@
 package com.backendengineer.springfordemo.Controller;
 
+import com.backendengineer.springfordemo.Service.MovieService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MovieControllerTest {
+    @Autowired
+    private MovieController movieController;
+    @MockBean
+    private MovieService movieService;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @BeforeEach
     void setUp() {
