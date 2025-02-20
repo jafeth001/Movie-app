ac6cb67078f19bfa663653f0c382bdf980ba6adb
jafeth001 <jafethadet2021@gmail.com>
2025-02-20 10:35:51 +0300

    add Unit testing

---
+++ springfordemo/src/test/java/com/backendengineer/springfordemo/Controller/MovieControllerTest.java
@@ -0,0 +1,29 @@
+package com.backendengineer.springfordemo.Controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addMovie() {
+    }
+
+    @Test
+    void findallmovie() {
+    }
+
+    @Test
+    void findbyid() {
+    }
+
+    @Test
+    void deletebyid() {
+    }
+}
